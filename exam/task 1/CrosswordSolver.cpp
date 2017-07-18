376842750bcb17f4ed795dfe7ad977b1c9e03ff8
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-18 13:42:26 +0300

    Update CrosswordSolver.cpp

--- exam/task 1/CrosswordSolver.cpp
+++ exam/task 1/CrosswordSolver.cpp
@@ -1,8 +1,9 @@
 #include"CrosswordSolver.h"
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<vector>
+#include<algorithm>
 
 std::string int_to_str(const int x)
 {
@@ -61,4 +62,49 @@ void CrosswordSolver::getAllWithInit(const std::string letter)
 			file << it << "\n";
 	}
 	file.close();
+}
+
+
+void CrosswordSolver::getAnagrams(const std::string word)
+{
+	setlocale(0, "");
+	std::string FullName = "Anagrams_" + word + ".txt";
+	std::ofstream file(FullName);
+	if (!file.is_open())
+		std::cout << "File: " << FullName << " not found\n";
+
+	for (auto it : temp)
+	{
+		if (it.length() == word.length())
+		{
+			if (std::is_permutation(it.begin(), it.end(), word.begin()))
+				file << it<<"\n";	
+		}
+	}
+	file.close();
+}
+
+
+
+void CrosswordSolver::findByMask(std::string x)
+{
+	//с?д
+	std::vector<char> alf = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+	for (auto it : temp)
+	{
+		
+		if (auto index = x.find('?') == true)
+		{
+			if (it.length() == x.length())
+			{
+				for (auto it2 : alf)
+				{
+					x[index] = it2;
+					if (x == it)
+						std::cout << it <<" ";//std::cout << x <<"\n";
+				}			
+			}
+		}
+	}
+
 }
