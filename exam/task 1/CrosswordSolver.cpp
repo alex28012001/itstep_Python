ebc0176d4cd4320160ae6bcea0a1aafa28377109
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-20 17:02:49 +0300

    Update CrosswordSolver.cpp

--- exam/task 1/CrosswordSolver.cpp
+++ exam/task 1/CrosswordSolver.cpp
@@ -101,54 +101,50 @@ void CrosswordSolver::findByMask(std::string x)
 	//с?д
 	std::vector<char> alf = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
 	std::vector<std::string> obj;
-
 	for (auto it : temp)
 	{
-		if (auto index = x.find('?') == true)
-		{
-			if (it.length() == x.length())
-			{
-				for (auto it2 : alf)
-				{
-					x[index] = it2;
-					obj.push_back(x);
-				}
-			}
-		}
-
-		for (auto it2 : obj)
-		{
-			if(it.length() == it2.length())
-			{
-				if(std::equal(it.begin(), it.end(), it2.begin())== true)
-					std::cout << it << "\n";
-			}
-		}
+	if (auto index = x.find('?') == true)
+	{
+	if (it.length() == x.length())
+	{
+	for (auto it2 : alf)
+	{
+	x[index] = it2;
+	obj.push_back(x);
+	}
+	}
+	}
+	for (auto it2 : obj)
+	{
+	if(it.length() == it2.length())
+	{
+	if(std::equal(it.begin(), it.end(), it2.begin())== true)
+	std::cout << it << "\n";
+	}
+	}
 	}
 	*/
 
 	std::map<std::string, int> obj;
-		for (auto it : temp)
-			obj.insert(std::pair<std::string, int>(it,it.length()));
-	
-		if (auto index = x.find('*') == true)
+	for (auto it : temp)
+		obj.insert(std::pair<std::string, int>(it, it.length()));
+
+	if (auto index = x.find('*') == true)
+	{
+		for (auto it = obj.begin(); it != obj.end(); ++it)
 		{
-			for (auto it = obj.begin(); it != obj.end(); ++it)
+			if (it->second == x.length())
 			{
-				if (it->second == x.length())
-				{
-					auto a = std::find(obj.begin(),obj.end(),'*');
-					std::cout << obj.find(a)<<"\n";
-				}
+				if(it->first.begin(), it->first[index])
+					std::cout <<it->first << "  " << it->second << "\n";
+
+				
 			}
 		}
+	}
+	//print_map(obj);
+	
 
-		print_map(obj);
-			
 
-		
+
 }
-	
-
-
-	
