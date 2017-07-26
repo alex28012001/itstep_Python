1b44bb1acb58b41a78969e14bed93a56b371c7f0
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-26 17:49:28 +0300

    Update Source.cpp

--- exam/task 1/Source.cpp
+++ exam/task 1/Source.cpp
@@ -1,22 +1,11 @@
 #include"CrosswordSolver.h"
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-
 int main()
 {
 	setlocale(0, "");
 	CrosswordSolver obj;
-	//obj.getAllWithLen(7);
-	//obj.getAllWithInit("х");
-	//obj.getAnagrams("дырка");
-	obj.findByMask("с?д");
+	obj.getAllWithLen(14);
+	obj.getAllWithInit("х");
+	obj.getAnagrams("апельсин");
 
-	
-
-		
-	
-	
 	return 0;
 }
