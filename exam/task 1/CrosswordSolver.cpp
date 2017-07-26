794d47c297641eabfbf835d9b25b55818b915a2d
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-07-26 17:49:51 +0300

    Update CrosswordSolver.cpp

--- exam/task 1/CrosswordSolver.cpp
+++ exam/task 1/CrosswordSolver.cpp
@@ -3,7 +3,6 @@
 #include<string>
 #include<fstream>
 #include<vector>
-#include<map>
 #include<algorithm>
 
 
@@ -42,7 +41,7 @@ void CrosswordSolver::getAllWithLen(const int value)
 		std::cout << "File: " << FullName << " not found\n";
 
 
-	for (auto it : temp)
+	for (auto &it : temp)
 	{
 		if (it.length() == value)
 			file << it << "\n";
@@ -59,7 +58,7 @@ void CrosswordSolver::getAllWithInit(const std::string letter)
 	if (!file.is_open())
 		std::cout << "File: " << FullName << " not found\n";
 
-	for (auto it : temp)
+	for (auto &it : temp)
 	{
 		if (it[0] == letter[0])
 			file << it << "\n";
@@ -71,12 +70,13 @@ void CrosswordSolver::getAllWithInit(const std::string letter)
 void CrosswordSolver::getAnagrams(const std::string word)
 {
 	setlocale(0, "");
+	
 	std::string FullName = "Anagrams_" + word + ".txt";
 	std::ofstream file(FullName);
 	if (!file.is_open())
 		std::cout << "File: " << FullName << " not found\n";
 
-	for (auto it : temp)
+	for (auto &it : temp)
 	{
 		if (it.length() == word.length())
 		{
@@ -88,63 +88,3 @@ void CrosswordSolver::getAnagrams(const std::string word)
 }
 
 
-void print_map(std::map<std::string, int> obj)
-{
-	for (auto it = obj.begin(); it != obj.end(); ++it)
-		std::cout << it->first << "  " << it->second << "\n";
-}
-
-
-void CrosswordSolver::findByMask(std::string x)
-{
-	/*
-	//с?д
-	std::vector<char> alf = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
-	std::vector<std::string> obj;
-	for (auto it : temp)
-	{
-	if (auto index = x.find('?') == true)
-	{
-	if (it.length() == x.length())
-	{
-	for (auto it2 : alf)
-	{
-	x[index] = it2;
-	obj.push_back(x);
-	}
-	}
-	}
-	for (auto it2 : obj)
-	{
-	if(it.length() == it2.length())
-	{
-	if(std::equal(it.begin(), it.end(), it2.begin())== true)
-	std::cout << it << "\n";
-	}
-	}
-	}
-	*/
-
-	std::map<std::string, int> obj;
-	for (auto it : temp)
-		obj.insert(std::pair<std::string, int>(it, it.length()));
-
-	if (auto index = x.find('*') == true)
-	{
-		for (auto it = obj.begin(); it != obj.end(); ++it)
-		{
-			if (it->second == x.length())
-			{
-				if(it->first.begin(), it->first[index])
-					std::cout <<it->first << "  " << it->second << "\n";
-
-				
-			}
-		}
-	}
-	//print_map(obj);
-	
-
-
-
-}
